75a79e00190bc3f61fe73c2860e21876b85d62dd
David Jenerette <dnj46591@email.vccs.edu>
2025-09-09 19:42:43 -0400

    Done

---
+++ Week 2 Assignment/Week 2 Assignment/Program.cs
@@ -0,0 +1,5 @@
+﻿// Week 2 Assignment
+Console.WriteLine("Hello");
+Console.WriteLine("Who is being seen today?");
+string PatientName = Console.ReadLine();
+Console.WriteLine("Welcome: " + PatientName);
