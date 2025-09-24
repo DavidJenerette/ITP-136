b901b1c56655d964c1febbaa44a85af5bb21ee53
David Jenerette <dnj46591@email.vccs.edu>
2025-09-23 23:55:15 -0400

    Finished

---
+++ Module 4 Assignment/Module 4 Assignment/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.ComponentModel.Design;
+using System.Numerics;
+
+namespace Module_4_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int option;
+            double AdultApp = 75, AdultCU = 100, ChildCU = 75, ChildApp = 50, Labs = 25, total, subtotal;
+            Console.WriteLine("Hello");
+            Console.WriteLine("Please Select an option");
+            Console.WriteLine("1 - Sick Appointment, 2 - Check-up");
+            option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    option = 1;
+                    Console.WriteLine("You chose a Sick Appointment");
+                    break;
+                case 2:
+                    option = 2;
+                    Console.WriteLine("You chose a Check-up");
+                    break;
+
+            }
+            if (option == 1)
+            {
+                Console.WriteLine("Is the patient an Adult or Child");
+                Console.WriteLine("3 - Adult ($75), 4 - Child ($50)");
+                option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 3:
+                        option = 3;
+                        subtotal = AdultApp;
+                        Console.WriteLine("Adult Fee = {0}", subtotal.ToString("C"));
+                        Console.WriteLine("Were labs done during the visit?");
+                        Console.WriteLine("5 - Yes (+$25), 6 - No");
+                        option = Convert.ToInt32(Console.ReadLine());
+
+                        switch (option)
+                        {
+                            case 5:
+                                option = 5;
+                                total = subtotal + Labs;
+                                Console.WriteLine("This is your bill = {0}", total.ToString("C"));
+                                break;
+                            case 6:
+                                option = 6;
+                                total = subtotal;
+                                Console.WriteLine("This is your bill = {0}", total.ToString("C"));
+                                break;
+                        }
+                        break;
+                    case 4:
+                        option = 4;
+                        subtotal = ChildApp;
+                        Console.WriteLine("Child Fee = {0}", subtotal.ToString("C"));
+                        Console.WriteLine("Were labs done during the visit?");
+                        Console.WriteLine("5 - Yes (+$25), 6 - No");
+                        option = Convert.ToInt32(Console.ReadLine());
+                        switch (option)
+                        {
+                            case 5:
+                                option = 5;
+                                total = subtotal + Labs;
+                                Console.WriteLine("This is your bill = {0}", total.ToString("C"));
+                                break;
+                            case 6:
+                                option = 6;
+                                total = subtotal;
+                                Console.WriteLine("This is your bill = {0}", total.ToString("C"));
+                                break;
+
+                        }
+                        break;
+                }
+            }
+            if (option == 2)
+            {
+                Console.WriteLine("Is the patient an Adult or Child");
+                Console.WriteLine("7 - Adult ($100), 8 - Child ($75)");
+                option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 7:
+                        total = AdultCU;
+                        Console.WriteLine("This is your bill = {0}", total.ToString("C"));
+                        break;
+                    case 8:
+                        total = ChildCU;
+                        Console.WriteLine("This is your bill = {0}", total.ToString("C"));
+                        break;
+                }
+
+            }
+        }
+    }
+}
---
+++ Week 4 CW/Week 4 CW/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.ComponentModel.Design;
+
+namespace Week_4_CW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //if and nested if
+            Console.WriteLine("Hello, World!");
+            int first = 5;
+            int second = 3;
+            if (first == second)
+            {
+                Console.WriteLine("These numbers are equal");
+            }
+            else
+            {
+                if (first > second)
+                {
+                    Console.WriteLine("The first number is greater than the second");
+                }
+            }
+            //Sequence if
+            char ch = 'i';
+            if (ch == 'A' || ch == 'a')
+            {
+                Console.WriteLine("vowel");
+            }
+            else if (ch == 'E' || ch == 'e')
+            {
+                Console.WriteLine("vowel");
+            }
+            else if (ch == 'I' || ch == 'i')
+            {
+                Console.WriteLine("vowel");
+            }
+            else if (ch == 'O' || ch == 'o')
+            {
+                Console.WriteLine("vowel");
+            }
+            else if (ch == 'U' || ch == 'u')
+            {
+                Console.WriteLine("vowel");
+            }
+            else
+            {
+                Console.WriteLine("Consonant");
+            }
+
+            //switch statement
+            int option, a, b, total;
+                Console.WriteLine("Please enter your first number");
+                a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter your second number");
+                b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("1 - add numbers, 2 - subtract, 3 - multiply, 4 - divide");
+            option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    total = (a + b);
+                    Console.WriteLine("The numbers added are {0} ", total);
+                    break;
+                case 2:
+                    total = (a - b);
+                    Console.WriteLine("The numbers subtracted are ", total);
+                    break;
+                case 3:
+                    total = (a * b);
+                    Console.WriteLine("The numbers multiplied are ", total);
+                    break;
+                case 4:
+                    total = (a / b);
+                    Console.WriteLine("The numbers divided are ", total);
+                    break;
+                default:
+                    Console.WriteLine("You picked a wrong number");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
