038f9dfbf4009d75137ee989f902d75621cc0728
David Jenerette <dnj46591@email.vccs.edu>
2025-10-01 19:09:11 -0400

    Done

---
+++ Week 6 Assignment/Week 6 Assignment/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.NetworkInformation;
+using System.Reflection.PortableExecutable;
+
+namespace Week_6_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WelcomeStatement();
+            Console.ReadKey();
+            MagicNumber();
+            Console.ReadKey();
+            FindArea();
+            Console.ReadKey();
+            Console.WriteLine("Your local tax rate is {0} ", LocalTaxRate() + ("%"));
+            Console.ReadKey();
+        }
+
+        static void WelcomeStatement()
+        {
+            Console.WriteLine("Welcome to My Method Examples");
+        }
+
+        static int MagicNumber()
+        {
+            int SecretNumber = 82;
+            Console.WriteLine("The magic number is {0} ", SecretNumber);
+            return SecretNumber;
+        }
+
+        static void FindArea()
+        {
+            int x, y, total;
+            Console.WriteLine("Please enter the first number");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the second number");
+            y = Convert.ToInt32(Console.ReadLine());
+            total = x * y;
+            Console.WriteLine("Your total is {0} ", total);
+        }
+
+        static int LocalTaxRate()
+        {
+            int LocalTax;
+            Console.WriteLine("Please enter your local tax rate");
+            LocalTax = Convert.ToInt32(Console.ReadLine());
+            return LocalTax;
+        }
+    }
+}
---
+++ Week 6 CW/Week 6 CW/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Week_6_CW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            WelcomeMessage();
+            for (int k = 0; k < 3; k++)
+            {
+                a = AskNumber();
+                b = AskNumber();
+                AddNumbers(a, b);
+            }
+            Console.WriteLine("Thank you for using the calculator");
+            Console.ReadKey();
+        }
+
+        static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to my calculator");
+
+        }
+
+        static int AskNumber()
+        {
+            int x;
+            Console.WriteLine("Please enter a number");
+            x = Convert.ToInt32(Console.ReadLine());
+            return x;
+        }
+        
+        static void AddNumbers(int num1, int num2)
+        {
+            int total = num1 + num2;
+            Console.WriteLine("Your total is {0} ", total);
+        }
+    }
+}
