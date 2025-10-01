da508ef0abc8cf6bff21ced560e581e4d446178d
David Jenerette <dnj46591@email.vccs.edu>
2025-09-30 20:01:23 -0400

    Finished Loop

---
+++ Week 5 Assignment/Week 5 Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Week_5_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int StartingNumber = 0, EndingNumber = 100, Counter = 0;
+            Console.WriteLine("Welcome to the Counter");
+            while(StartingNumber <= EndingNumber) 
+            {
+                Console.WriteLine("The counter has currently counted: " + Counter++ + (" times"));
+                Console.WriteLine(StartingNumber++ + (" Sheep."));
+                Console.ReadKey();
+               
+            }
+            Console.WriteLine("You Reached " + EndingNumber + ("!!!"));
+            Console.ReadKey();
+        } 
+    }
+}
---
+++ Week 5 CW/Week 5 CW/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+
+namespace Week_5_CW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, total;
+            Console.WriteLine("Hello, World!");
+            a = 0;
+            b = 5;
+            while (a <= b)
+            {
+                Console.WriteLine(a);
+                a++;
+
+            }
+            for (int k = 0; k < 3; k++)
+            {
+
+                Console.WriteLine(k);
+            }
+            Console.WriteLine("Welcome to the calculator");
+            for (int k = 0; k < 5; k++)
+            {
+                Console.WriteLine("Enter the first number");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the second number");
+                b = Convert.ToInt32(Console.ReadLine());
+                total = a + b;
+                Console.WriteLine("This is your total " + total);
+                Console.ReadKey();
+            }
+        }
+    }
+}
