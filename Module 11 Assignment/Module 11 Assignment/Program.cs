a16c9dbb2b25132556518ead3df687ef5a0963c2
David Jenerette <dnj46591@email.vccs.edu>
2025-11-11 22:15:58 -0500

    Finished

---
+++ Module 11 Assignment/Module 11 Assignment/Program.cs
@@ -0,0 +1,80 @@
+﻿
+namespace Module_11_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal x, y;
+            
+            decimal total;
+            int option;
+            Console.WriteLine("Please input a number for variable 'x'");
+            x = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Please input a number for variable 'y'");
+            y = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Please select which operation you want to use");
+            Console.WriteLine("1: Addition, 2: Subtraction, 3: Multiplication, 4: Division");
+            option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    option = 1;
+                    try
+                    {
+                        total = x + y;
+                        Console.WriteLine("This is your total " + total);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("There was an error with your input");
+                        Console.WriteLine(e.ToString());
+                    }
+                    Console.ReadKey();
+                    break;
+                case 2:
+                    option = 2;
+                    try
+                    {
+                        total = x - y;
+                        Console.WriteLine("This is your total " + total);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("There was an error with your input");
+                        Console.WriteLine(e.ToString());
+                    }
+                    Console.ReadKey();
+                    break;
+                case 3:
+                    option = 3;
+                    try
+                    {
+                        total = x * y;
+                        Console.WriteLine("This is your total " + total);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("There was an error with your input");
+                        Console.WriteLine(e.ToString());
+                    }
+                    Console.ReadKey();
+                    break;
+                case 4:
+                    option = 4;
+                    try
+                    {
+                        total = x / y;
+                        Console.WriteLine("This is your total " + total);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("There was an error with your input");
+                        Console.WriteLine(e.ToString());
+                    }
+                    Console.ReadKey();
+                    break;
+            }
+        }
+    }
+}
---
+++ Week 11 CW/Week 11 CW/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Week_11_CW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int num1, num2;
+            double total;
+            Console.WriteLine("Please enter the first number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the second number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                total = num1 / num2;
+                Console.WriteLine("This is your total " + total);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("You cannot divide by zero");
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadKey();
+
+            string[] numberArray = { "Gretsky", "Jordan", "Ruth" };
+            Console.WriteLine("Enter the element you want to see");
+            int elementNumber = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Console.WriteLine(numberArray[elementNumber]);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("You must enter a number between 0-2");
+                Console.WriteLine("Exception Caught: " + e.ToString());
+            }
+        }
+    }
+}
